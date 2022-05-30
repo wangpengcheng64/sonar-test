71656bdb5cbf65806231019f97d6b3797a0a821b
op2213 <pons.wang@china-entercom.net>
2022-05-30 17:11:32 +0800

    first commit

---
+++ src/main/java/com/example/sonartest/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.sonartest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Demo class
+ *
+ * @author Pons Wang
+ * @version 1.0.0
+ * @date 2022/5/30 16:25
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello test";
+    }
+}
